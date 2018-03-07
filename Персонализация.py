d93d876cd6ba0fb75ff02a113647a0a5dec41232
Luv H8R <36643761+luvh8r@users.noreply.github.com>
2018-03-07 22:50:25 +0300

    Delete Персонализация.py

--- Персонализация.py
+++
@@ -1,8 +0,0 @@
-from bottle import route, run, template
-
-@route('/')
-@route('/hello/<name>')
-def index(name="Stranger"):
-    return template('hello_template', name=name)
-
-run(host='localhost', port=8080)
